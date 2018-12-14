a21fa5bd7fffcc34dd59f642e1cd249bbc63126c
DruiDsta <33695583+DruiDsta@users.noreply.github.com>
2018-12-14 13:59:08 +0200

    Add files via upload

--- P_Lab_5/dru_lib_body.cpp
+++ P_Lab_5/dru_lib_body.cpp
@@ -1,17 +1,8 @@
 #include "dru_lib_body.h"
 
-void body::body(string *K){
-	this->kind=K;
+void body::body(){
 	this->size_body=start_size_body;
-	this->Speed=start_speed;
-}
-void istream &operator>>(istream &in, body &object){
-
-	return in;
-}
-void ostream &operator<<(ostream &out, body &object){
-
-	return out;
+	this->speed=normal_speed;
 }
 void body::set_size_body(float *size){
 	this->size_body=size;
@@ -24,6 +15,17 @@ void body::get_size_body(){
 }
 void body::get_Speed(){
 	return this->Speed;
+}
+istream &operator>>(istream &in, body &object){
+
+	return in;
+}
+ostream &operator<<(ostream &out, body &object){
+
+	return out;
+}
+point body::get_position(){
+	return this->position;
 }
 
 // lolololololololololololololololololollolololo
@@ -60,10 +62,27 @@ void opponent::opponent(float *_x, float *_y){
 }
 void opponent::randOpponent(){
 	switch(1+rand()%N_T){
-		case 1:{this->kind=toxic; break;}
-		case 2:{this->kind=speed; break;}
-		case 3:{this->kind=sniper; break;}
-		case 4:{this->kind=sredder; break;}
-		case 5:{this->kind=Anton; break;}
+		case 1:{this->kind=new toxic(); break;}
+		case 2:{this->kind=new speed(); break;}
+		case 3:{this->kind=new sniper(); break;}
+		case 4:{this->kind=new shredder(); break;}
+		case 5:{this->kind=new Anton(); break;}
+		case 6:{this->kind=new AIDS(); break;}
 	}
+}
+void toxic::toxic(){
+
+}
+void toxic::attack(body *OP){
+	if(dist_attack(this, OP))	OP->privilages.push_back(TOXIC);
+	body::attack(OP);
+}
+
+float distance(point *PL, point *OP){
+
+}
+bool dist_attack(body *PL, body *OP){
+	if(distance(PL->get_position(), OP->get_position())
+		<=(OP->size_body+PL->size_body))	return true;
+	else return false;
 }
--- P_Lab_5/dru_lib_body.h
+++ P_Lab_5/dru_lib_body.h
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <utility>
+#include "map_game.h"
 #include <SFML/Graphics.hpp>
 using namespace std;
 
@@ -15,7 +16,7 @@ using namespace std;
 #define pause system("pause")
 #endif
 #ifndef N_T
-#define N_T 5
+#define N_T 6
 #endif
 #ifndef size_food
 #define size_food 5
@@ -26,8 +27,8 @@ using namespace std;
 #ifndef normal_speed
 #define normal_speed 0.5
 #endif
-#ifndef toxic
-#define toxic "toxic"
+#ifndef TOXIC
+#define TOXIC "TOXIC"
 #endif
 #ifndef speed
 #define speed "speed"
@@ -41,6 +42,9 @@ using namespace std;
 #ifndef Anton
 #define Anton "Anton"
 #endif
+#ifndef AIDS
+#define AIDS "AIDS"
+#endif
 
 
 template <typename T>
@@ -50,18 +54,22 @@ class body
 	float size_body; // При получении урона размер уменьшаетсяи и из тела выпадает еда
 	float Speed;
 	float damage;
+	point position;
 protected:
-	string kind;
 	vector<string> privilages; // Пассивное действие на персонажа (яд, ...)
 public:
-	body(string *);
+	body();
 	~body();
 	void set_size_body(float *);
 	void set_Speed(float *);
-	void get_size_body();
-	void get_Speed();
-	void istream &operator>>(istream &, body &);
-	void ostream &operator<<(ostream &, body &);
+	void set_damage(float *);
+	float get_size_body();
+	float get_Speed();
+	float get_damage();
+	friend istream &operator>>(istream &, body &);
+	friend ostream &operator<<(ostream &, body &);
+	virtual void attack(body *);
+	point get_position();
 };
 class food
 {
@@ -71,20 +79,63 @@ public:
 	~food();
 	// есть задифайнина хрень size_food забей нужное значение туда и юзай size_food
 };
-class player : body //Нужно сделать функцию меню выбора персонажа
+class player //Нужно сделать функцию меню выбора персонажа
 {
-	point position;
+	body *kind;
 public:
 	player(float *, float *);
 	~player();
-	void istream &operator>>(istream &, player &);
-	void ostream &operator<<(ostream &, player &);
 };
-class opponent : body
+class opponent
 {
-	point position;
+	body *kind;
 public:
 	opponent(float *, float *);
 	~opponent()	{delete kind;}
 	void randOpponent();
 };
+// Рассы персонажей:
+// Нужно сделать, что-бы персонажи отличались визуально
+class toxic : body // Отравляет персонажа
+{
+public:
+	toxic();
+	~toxic();
+	void attack(body *);
+};
+class speed : body // Может ускорятся, но теряет немного массы
+{
+public:
+	speed();
+	~speed();
+	void UP_Speed(/*...*/);
+};
+class sniper : body
+{
+public:
+	sniper();
+	~sniper();
+	void attack(body *);
+};
+class shredder : body // Наносит много урона
+{
+public:
+	shredder();
+	~shredder();
+};
+class Anton
+{
+public:
+	Anton();
+	~Anton();
+	
+};
+class AIDS
+{
+public:
+	AIDS();
+	~AIDS();
+	
+};
+float distance(body *PL, body *OP);
+bool dist_attack(body *PL, body *OP);
